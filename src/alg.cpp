26d365efd0dd8d65b08198993300c0e410b9bbd1
pkonysheva <pkonyseva4@gmail.com>
2026-03-27 17:51:50 +0300

    Конышева 25-ИСТ-1-1 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,55 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+#include <iostream>
+
+int countPairs1(int* arr, int len, int value) {
+    int cnt = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {cnt++;}
+        }
+    }
+    return cnt;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int countPairs2(int* arr, int len, int value) {
+    int cnt = 0;
+    int l = 0;
+    int r = len - 1;
+
+    while (l < r) {
+        if (arr[l] + arr[r] == value) {
+            cnt++;
+            l++;
+            r--;
+        } else if (arr[l] + arr[r] < value) {
+            l++;+} else {
+            r--;
+        }
+    }
+    return cnt;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+
+int countPairs3(int* arr, int len, int value) {
+    int cnt = 0;
+
+    for (int i = 0; i < len; i++) {
+        int set = value - arr[i];
+
+        int l = i + 1;
+        int r = len - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] == set) {
+                cnt++;
+                break;
+            } else if (arr[mid] < set) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+    }
+    return cnt;
 }
