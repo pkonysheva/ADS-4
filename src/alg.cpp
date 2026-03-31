b1d497925d10756be42a159228c8daa27f4a4011
pkonysheva <pkonyseva4@gmail.com>
2026-03-31 16:58:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,9 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
 
 int countPairs1(int* arr, int len, int value) {
     int cnt = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
             if (arr[i] + arr[j] == value) {
                 cnt++;
             }
@@ -15,46 +14,62 @@ int countPairs1(int* arr, int len, int value) {
 
 int countPairs2(int* arr, int len, int value) {
     int cnt = 0;
-    int l = 0;
-    int r = len - 1;
+    for (int i = 0; i < len; ++i) {
+        int task = value - arr[i];
+        int left = i + 1;
+        int right = len - 1;
+        int pos = -1;
 
-    while (l < r) {
-        int sum = arr[l] + arr[r];
-        if (sum == value) {
-            if (arr[l] == arr[r]) {
-                int n = r - l + 1;
-                cnt += n * (n - 1) / 2;
-                break;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == task) {
+                pos = mid;
+                right = mid - 1;
+            } else if (arr[mid] < task) {
+                left = mid + 1;
             } else {
-                cnt++;
-                l++;
+                right = mid - 1;
             }
-        } else if (sum < value) {
-            l++;
-        } else {
-            r--;
+        }
+        if (pos != -1) {
+            int j = pos;
+            while (j < len && arr[j] == task) {
+                ++cnt;
+                ++j;
+            }
         }
     }
     return cnt;
 }
 
 int countPairs3(int* arr, int len, int value) {
-    int cnt = 0;
-    for (int i = 0; i < len; i++) {
-        int set = value - arr[i];
-        int l = i + 1;
-        int r = len - 1;
+    int left = 0, cnt = 0;
+    int right = len - 1;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (arr[mid] == set) {
-                cnt++;
+    while (left < right) {
+        if (arr[left] + arr[right] < value) {
+            ++left;
+        } else if (arr[left] + arr[right] > value) {
+            --right;
+        } else {
+            if (arr[left] == arr[right]) {
+                int n = right - left + 1;
+                cnt += n * (n - 1) / 2;
                 break;
-            } else if (arr[mid] < set) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
             }
+
+            int leftCnt = 0, leftv = arr[left];
+            while (left < right && arr[left] == leftv) {
+                ++left;
+                ++leftCnt;
+            }
+
+            int rightCnt = 0, rightv = arr[right];
+            while (left <= right && arr[right] == rightv) {
+                --right;
+                ++rightCnt;
+            }
+            cnt += leftCnt * rightCnt;
         }
     }
     return cnt;
